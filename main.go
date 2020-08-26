aa4145e607d6173af94df94c67c437f84a58b3f7
Nik Ogura <nik@orionlabs.io>
2020-08-26 14:33:11 -0700

    added copyright to all program files

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-// Copyright © 2020 Nik Ogura <nik@orionlabs.io>
+// Copyright © Copyright 2020 Orion Labs, Inc.
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
